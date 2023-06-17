01227bea94c02c1b93fce3b95e1379772244a678
ggorotiza1 <gabugoro17@gmail.com>
2023-06-17 16:18:06 -0500

    Responsivo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ChannelsComponent } from './components/channels/channels.component';
+import { FooterComponent } from './components/footer/footer.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'channels', component: ChannelsComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
